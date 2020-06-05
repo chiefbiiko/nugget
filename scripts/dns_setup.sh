14d0ab31cb4c66273d31d8819bc4fcba4603becc
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2020-06-05 10:54:56 +0200

    impl: dns init script docs for own sanity

--- scripts/dns_setup.sh
+++ scripts/dns_setup.sh
@@ -1,27 +1,51 @@
 #!/usr/bin/env bash
 
-set -uo pipefail
+set -o pipefail
 
-DOMAIN=$1
-TEST_ACCOUNT_ID=$2
-PROD_ACCOUNT_ID=$3
+function help {
+  printf "warning: run this once only for dns initialization!
 
-cat <<EOF
-warning: $0 is intended to be run once for initialization only!
-note: make sure the prod account $PROD_ACCOUNT_ID owns the domain $DOMAIN
-EOF
+note:
+  - make sure the prod account owns the prod domain
+    - TODO(chiefbiiko): transfer the prod domain to the fresh prod account
+  - the test account should be an unconfigured rookie
+  
+usage: %s [-h, --help]
+  --prod_domain PROD_DOMAIN
+  --test_domain TEST_DOMAIN
+  --test_account_id TEST_ACCOUNT_ID
+  --prod_account_id PROD_ACCOUNT_ID" "$(basename "$0")"
+  
+}
 
-# TODO: prep all zone and domain names carefully (prefix, trailiing dot, etc.)
-test_domain_name="test.$DOMAIN"
-prod_domain_name=$DOMAIN
-test_zone_name="$test_domain_name."
-prod_zone_name="$prod_domain_name."
+while [[ $# -ne 0 ]]; do case $1 in
+  --prod_domain) prod_domain=$2; shift 2;;
+  --test_domain) test_domain=$2; shift 2;;
+  --test_account_id) test_account_id=$2; shift 2;;
+  --prod_account_id) prod_account_id=$2; shift 2;;
+  -h|--help) help; exit 0;;
+esac; done
 
-# TODO: prep caller references
-prod_zone_caller_reference="prod_$(date +%s)"
-test_zone_caller_reference="test_$(date +%s)"
+if [[ -z "$prod_domain" ]] || [[ -z "$test_domain" ]] || 
+   [[ -z "$test_account_id" ]] || [[ -z "$prod_account_id" ]]; then
+  usage; exit 0;
+fi
+
+set -u
+
+exit
 
 # TODO: auth with test account & create a hosted zone for the test account and record its NS servers TODO
+aws sts assume-role \
+  --role-arn="arn:aws:iam::$test_account_id:role/OrganizationAccountAccessRole" \
+  --role-session-name=oaar
+
+# TODO: capture ns servers from HostedZone output structure
+# https://docs.aws.amazon.com/Route53/latest/DeveloperGuide/AboutHZWorkingWith.html
+aws route53 create-hosted-zone \
+  --name="$test_domain_name." \
+  --caller-reference="test_$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+  
 
 
 # TODO: auth with prod account & create a hosted zone (if not existing) for the prod account
